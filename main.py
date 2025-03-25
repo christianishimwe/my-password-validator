743314c64d477aa52690c42612f5a8b4ac56ad68
Christian Ishimwe <christianishimwe@ist2-10539.apn.wlan.private.upenn.edu>
2025-03-25 13:11:07 -0400

    finished the generator

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import flask
-
+import re
 
 # TODO: change this to your academic email
-AUTHOR = "lumbroso@seas.upenn.edu"
-
+AUTHOR = "ishimwe@seas.upenn.edu"
 
 app = flask.Flask(__name__)
 
-
 # This is a simple route to test your server
-
-
 @app.route("/")
 def hello():
     return f"Hello from my Password Validator! &mdash; <tt>{AUTHOR}</tt>"
 
-
-# This is a sample "password validator" endpoint
-# It is not yet implemented, and will return HTTP 501 in all situations
-
-
+# Password validator endpoint
 @app.route("/v1/checkPassword", methods=["POST"])
 def check_password():
     data = flask.request.get_json() or {}
     pw = data.get("password", "")
 
-    # FIXME: to be implemented
-    return flask.jsonify({"valid": False, "reason": "Not implemented"}), 501
+    # Check: Length
+    if len(pw) < 8:
+        return flask.jsonify({"valid": False, "reason": "Password must be at least 8 characters long"}), 400
+
+    # Check: At least 2 uppercase letters
+    if len(re.findall(r"[A-Z]", pw)) < 2:
+        return flask.jsonify({"valid": False, "reason": "Password must contain at least 2 uppercase letters"}), 400
+
+    # Check: At least 1 digit
+    if not re.search(r"\d", pw):
+        return flask.jsonify({"valid": False, "reason": "Password must contain at least 1 digit"}), 400
+
+    # Check: At least 1 special character from !@#$%^&*
+    if not re.search(r"[!@#$%^&*]", pw):
+        return flask.jsonify({"valid": False, "reason": "Password must contain at least 1 special character (!@#$%^&*)"}), 400
+
+    # If all checks pass
+    return flask.jsonify({"valid": True, "reason": "Password is valid"}), 200
